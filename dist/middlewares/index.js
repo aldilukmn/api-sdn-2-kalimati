e88cd4ebf6e46c20d6b60063db428b04ffc15bef
aldilukmn <aldilukman99@gmail.com>
2026-06-30 13:55:34 +0700

    fix handle role for teacher

--- dist/middlewares/index.js
+++ dist/middlewares/index.js
@@ -54,8 +54,8 @@ class UserMiddleware {
         }
         catch (e) {
             if (e instanceof Error) {
-                const response = (0, utils_1.defaultResponse)(401, 'fail', e.message);
-                res.status(401).json(response);
+                const response = (0, utils_1.defaultResponse)(403, 'fail', e.message);
+                res.status(403).json(response);
             }
         }
     };
@@ -74,8 +74,8 @@ class UserMiddleware {
         }
         catch (e) {
             if (e instanceof Error) {
-                const response = (0, utils_1.defaultResponse)(401, "fail", e.message);
-                res.status(401).json(response);
+                const response = (0, utils_1.defaultResponse)(403, "fail", e.message);
+                res.status(403).json(response);
             }
         }
     };
@@ -92,8 +92,8 @@ class UserMiddleware {
         }
         catch (e) {
             if (e instanceof Error) {
-                const response = (0, utils_1.defaultResponse)(401, 'fail', e.message);
-                res.status(401).json(response);
+                const response = (0, utils_1.defaultResponse)(403, 'fail', e.message);
+                res.status(403).json(response);
             }
         }
     };
@@ -110,8 +110,8 @@ class UserMiddleware {
         }
         catch (e) {
             if (e instanceof Error) {
-                const response = (0, utils_1.defaultResponse)(401, 'fail', e.message);
-                res.status(401).json(response);
+                const response = (0, utils_1.defaultResponse)(403, 'fail', e.message);
+                res.status(403).json(response);
             }
         }
     };
@@ -128,8 +128,8 @@ class UserMiddleware {
         }
         catch (e) {
             if (e instanceof Error) {
-                const response = (0, utils_1.defaultResponse)(401, 'fail', e.message);
-                res.status(401).json(response);
+                const response = (0, utils_1.defaultResponse)(403, 'fail', e.message);
+                res.status(403).json(response);
             }
         }
     };
